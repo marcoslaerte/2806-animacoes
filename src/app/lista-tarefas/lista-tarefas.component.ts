2f090c9eaa9fdcc3383e03a1fa04e67dafffc8ca
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-11-13 14:57:08 -0300

    Separando a animação em um arquivo diferente e importando-a no arquivo .ts

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,27 +4,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { highlightedStateTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [trigger('highlightedState', [
-    state('default', style({
-      border: '2px solid #B2B6FF' // Pode-se passar 'background-color' (com aspas simples) ou backgroundColor sem aspas simples. Mas o valor é sempre entre aspas simples.
-    })),
-    state('highlighted', style({
-      border: '4px solid #B2B6FF',
-      filter: 'brightness(92%)'
-    })),
-    transition('default => highlighted', [
-      animate('200ms ease-out',  style({
-        transform: 'scale(1.02)'
-      })),
-      animate(200) //outras formas: '200ms' '2s'
-    ])
-  ])]
+  animations: [highlightedStateTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
