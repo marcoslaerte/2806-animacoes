0e9d897ae8d14ddfcc5d54dffb87b649b8e2b0a7
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-11-13 18:08:18 -0300

    Adicionando efeito em um componente que não aparece na tela e depois aparece utilizando o estado reservado void

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -14,4 +14,23 @@ export const highlightedStateTrigger = trigger('highlightedState', [
     })),
     animate(200) //outras formas: '200ms' '2s'
   ])
-])
+]);
+
+export const shownStateTrigger = trigger('shownState', [
+  state('shown', style({
+
+  })),
+  transition('void => shown', [
+    style({
+      opacity: 0
+    }),
+    animate(300, style({
+      opacity: 1
+    }))
+  ]),
+  transition('shown => void', [
+    animate(300, style({
+      opacity: 0
+    }))
+  ])
+]);
